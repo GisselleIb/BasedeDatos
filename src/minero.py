c53c9d71bca3c696ee1e5b2bc8c05c58d77761c2
Gisselle <gisselleib@ciencias.unam.mx>
2018-10-03 21:24:44 -0500

    Creado el minero junto con sus pruebas unitarias

--- src/minero.py
+++ src/minero.py
@@ -1,12 +1,31 @@
 from basededatos import BaseDeDatos
-import sys
+from pathlib import Path
+import eyed3
 class Minero:
 
-    def minar():
-        pass
-        
-    def creaCancion(listTag):
-        pass
+    def __init__():
+        self.path=str(Path.home())+'/Music'
+        self.listCancion=[]
 
-    def llenaBase(s):
+    def minar(dir):
+        audiofile=""
+        path=Path(dir)
+        for mp3 in path.iterdir():
+            if(mp3.is_dir()):
+                self.minar(str(mp3))
+            audiofile=eyed3.load(mp3)
+            self.listCancion.append(self.creaCancion(audiofile,path))
+        self.creaRegistros()
+
+    def creaCancion(audiofile,path):
+        cancion=Cancion()
+        cancion.setArtista(audiofile.tag.artist)
+        cancion.setTitulo(audiofile.tag.title)
+        cancion.setGenero(str(audiofile.tag.genre))
+        cancion.setTrack(audiofile.tag.track_num)
+        cancion.setAlbum(audiofile.tag.album)
+        cancion.setPath(path)
+        return cancion
+
+    def creaRegistros():
         pass
--- test/testMinero.py
+++ test/testMinero.py
@@ -1,14 +1,20 @@
 import unittest
-from src.minero import minero
-
+from src.minero import Minero
+from src.cancion import Cancion
 class TestMinero(unittest.TestCase):
     minero=Minero()
 
     def test_minar():
         pass
 
-    def test_creaCancion(listTag):
-        pass
+    def test_creaCancion():
+        audiotest=''
+        c=None
+        path=str(self.minero.path)+'/Music'
+        p=Path(dir)
+        for mp3 in path.iterdir():
+            audiotest=eyed3.load(mp3)
+            self.assertTrue(type(self.minero.creaCancion(audiotest)) is type(Cancion))
 
     def test_llenaBase():
         pass
